132ee06e5c3bfbea34092afec16ecd15291acbc9
suyashshinde2040K <112718489+suyashshinde2040K@users.noreply.github.com>
2022-09-02 22:29:12 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+import 'package:untitled/login.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    initialRoute: 'login',
+    routes: {
+    'login': (context) =>MyLogin()},
+  ));
+}
